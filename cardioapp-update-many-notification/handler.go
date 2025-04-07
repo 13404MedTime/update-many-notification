3036a9313ce0cf1f6b3d2de188648c521428708a
ibrokhimov.fr <fr.ibrokhimov@gmail.com>
2025-04-07 15:18:15 +0500

    Define structs for handling API responses and request bodies

---
+++ cardioapp-update-many-notification/handler.go
@@ -0,0 +1,70 @@
+package function
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+// Datas This is response struct from create
+type Datas struct {
+	Data struct {
+		Data struct {
+			Data map[string]interface{} `json:"data"`
+		} `json:"data"`
+	} `json:"data"`
+}
+
+// ClientApiResponse This is get single api response
+type ClientApiResponse struct {
+	Data ClientApiData `json:"data"`
+}
+
+type ClientApiData struct {
+	Data ClientApiResp `json:"data"`
+}
+
+type ClientApiResp struct {
+	Response map[string]interface{} `json:"response"`
+}
+
+type Response struct {
+	Status string                 `json:"status"`
+	Data   map[string]interface{} `json:"data"`
+}
+
+// NewRequestBody's Data (map) field will be in this structure
+//.   fields
+// objects_ids []string
+// table_slug string
+// object_data map[string]interface
+// method string
+// app_id string
+
+// but all field will be an interface, you must do type assertion
+
+type HttpRequest struct {
+	Method  string      `json:"method"`
+	Path    string      `json:"path"`
+	Headers http.Header `json:"headers"`
+	Params  url.Values  `json:"params"`
+	Body    []byte      `json:"body"`
+}
+
+type AuthData struct {
+	Type string                 `json:"type"`
+	Data map[string]interface{} `json:"data"`
+}
+
+type NewRequestBody struct {
+	RequestData HttpRequest            `json:"request_data"`
+	Auth        AuthData               `json:"auth"`
+	Data        map[string]interface{} `json:"data"`
+}
+type Request struct {
+	Data map[string]interface{} `json:"data"`
+}
